70d6b67361bc782d402d313588365af5d68917f0
rajamuhammad <rajamuhammad53@gmail.com>
2017-12-12 23:14:35 +0500

    Good

---
+++ WebApplication9/Controllers/BatchController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MiniAttandanceSystem.Controllers
+{
+    public class BatchController : ApiController
+    {
+        Database1Entities db = new Database1Entities();
+
+        [Route("api/GetAllBatches")]
+        public List<Batch> GetAllBatches()
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            return db.Batches.ToList();
+        }
+
+        
+        [Route("api/GetStudntsByBatch/{batch}")]
+        public List<Student> GetStudntsByBatch(string batch)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            return db.Students.Where(s => s.Batch.BatchCode == batch).ToList();
+        }
+    }
+}
